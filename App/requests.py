03fe32eeeb77aa059e8ec262271791375c44388c
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-08 22:01:22 +0300

    add configure_requests function declaring api and url

---
+++ App/requests.py
@@ -0,0 +1,23 @@
+import urlib2.request
+import json
+from .models import Sources, Articles
+
+# Getting the API KEY
+api_key = None
+
+# Getting the news base url
+base_url = None
+
+
+def configure_request(app):
+    global api_key, base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+
+
+
+
+
+
+
+
