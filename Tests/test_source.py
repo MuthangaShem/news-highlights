3fe2003d388c5cd67715eedcb171d77e1fd43294
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-08 23:02:06 +0300

    add sourcetest: define setUp function

---
+++ Tests/test_source.py
@@ -0,0 +1,18 @@
+import unittest
+from app.models import Sources
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test class to test behaviours of the Sources class
+
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
+
+    def setUp(self):
+        """ Set up method to run before each test case """
+        self.news_source = Sources('abc-news-au','ABC News (AU)', 'Australia\'s most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.',"http://www.abc.net.au/news","general")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
