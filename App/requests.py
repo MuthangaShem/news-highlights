8b399ede90512fbf588020d108351f91e842f33a
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-08 22:02:51 +0300

    add function that retrieves news sources list from news API

--- App/requests.py
+++ App/requests.py
@@ -15,7 +15,24 @@ def configure_request(app):
     base_url = app.config['NEWS_API_BASE_URL']
 
 
+def get_sources(source):
+    """
+    Retrieve news sources list from the News api
+    """
 
+    get_sources_url = base_url.format(source, api_key)
+
+    with urlib2.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
 
 
 
