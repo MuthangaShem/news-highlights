e43d75c0b367d4da9f2c47b94617220c0d5b1f9c
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-08 22:59:17 +0300

    add test: define setUp function

---
+++ Tests/test_article.py
@@ -0,0 +1,28 @@
+import unittest
+from .models import Articles
+
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test class to test behaviours of the Article class
+
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test case
+        '''
+        self.new_article = Articles('CNN News',
+                                    'Obama set to visit Kenya',
+                                    'Kenya will host Potus on the 3rd to 5th of November this year ',
+                                    'https://ichef.cnnmobile.com/news/1024/cpsprodpb/F046/production/_98901516_2efffed4-d4a6-486a-8a78-112232b92faa.jpg',
+                                    'http://www.cnn.com/news/world-us-42420150',
+                                    '2018-10-05T09:14:56Z')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
