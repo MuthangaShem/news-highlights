f47fe094f10008aeb8ef95da23b503d562776dfe
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-08 23:23:11 +0300

    add articles root

--- App/Main/views.py
+++ App/Main/views.py
@@ -13,3 +13,11 @@ def index():
     return render_template('index.html', title=title, sources=news_sources)
 
 
+@main.route('/articles/<source_id>')
+def articles(source_id):
+    '''
+    Function that returns a source page and its data, view page source
+    '''
+    title = f"{source_id} page"
+    articles = get_articles(source_id)
+    return render_template('articles.html', title=title, articles=articles)
