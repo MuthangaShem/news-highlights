f62a6ed663d1cd723149d4b31acce2a93765cdb6
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-08 23:03:34 +0300

    add sourcetest: test_init function

--- Tests/test_source.py
+++ Tests/test_source.py
@@ -19,5 +19,13 @@ class SourcesTest(unittest.TestCase):
         '''
         self.assertTrue( isinstance( self.news_source, Sources ) )
 
+    def test_init(self):
+        '''
+        Test case to check if the Source class is initialised
+        '''
+        self.assertEqual( self.news_source.id, 'abc-news-au')
+        self.assertEqual( self.news_source.name, 'ABC News (AU)')
+        self.assertEqual( self.news_source.description, 'Australia\'s most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.')
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
